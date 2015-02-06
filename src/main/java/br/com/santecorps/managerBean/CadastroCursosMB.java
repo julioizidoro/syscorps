e46faa5bba34d7526cf7fefaf98e464d278f8c5c
Wolverine <Wolverine@Wolverine-DELL>
2015-02-05 22:09:56 -0200

    Criando telas cadastro

---
+++ src/main/java/br/com/santecorps/managerBean/CadastroCursosMB.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.santecorps.managerBean;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Wolverine
+ */
+
+@Named
+@SessionScoped
+public class CadastroCursosMB implements Serializable{
+    
+    public String novoCurso(){
+        return "cadcursos";
+    }
+    
+}
---
+++ src/main/java/br/com/santecorps/managerBean/MenuMB.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.santecorps.managerBean;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Wolverine
+ */
+@Named
+@SessionScoped
+public class MenuMB implements Serializable{
+    
+    public String cadastroCursos(){
+        return "jsf/cadastro/conscursos";
+    }
+    
+    public String cadastroTurmas(){
+        return "jsf/cadastro/consturmas";
+    }
+    
+}
