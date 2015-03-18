5522cd60e14d0dfb6afe8263c18e21fa6e71a26c
Kamila <Kamila@Kamila-PC>
2015-03-18 18:33:48 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/santecorps/managerBean/MensagemMB.java
@@ -0,0 +1,32 @@
+package br.com.santecorps.managerBean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+ 
+@ManagedBean
+public class MensagemMB {
+     
+    private String messagem;
+ 
+    public String getMessagem() {
+        return messagem;
+    }
+ 
+    public void setMessagem(String messagem) {
+        this.messagem = messagem;
+    }
+     
+    public void saveMessagem() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage("Cadastrado com Sucesso", ""));
+    }
+     public void excluiMessagem() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage("Excluido com Sucesso", ""));
+    }
+     public void matriculaMessagem() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage("Excluido com Sucesso", ""));
+    }
+}
